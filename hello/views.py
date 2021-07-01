b78531881c3fdb714140a52f79bcb0992f88987d
Camilo Castrillon <desarrollo@servitel.co>
2021-07-01 11:54:47 -0500

    Se revirtieron los cambios

--- hello/views.py
+++ hello/views.py
@@ -2,4 +2,4 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def hello(request):
-    return HttpResponse("Hola mundo desde Azure test 1!!")
+    return HttpResponse("Hola mundo desde Azure!")
